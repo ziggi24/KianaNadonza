a0b35244c1ad80e476ede7e8a90857de7fab3775
bee <ziggi24@gmail.com>
2024-01-13 16:00:17 -0700

    fixing bubbles on mobile

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,24 @@
 console.log('connected!');
 
+cv = document.getElementById('cv')
+pr = document.getElementById('pr')
+uo = document.getElementById('uo')
+console.log(cv)
+
+
+cv.addEventListener('click', function (e) {
+    console.log('listener')
+    e.preventDefault();
+    window.location = '/assets/KianaNadonzaCV.pdf';
+}, false);
+pr.addEventListener('click', function (e) {
+    console.log('listener')
+    e.preventDefault();
+    window.location = '#';
+}, false);
+uo.addEventListener('click', function (e) {
+    console.log('listener')
+    e.preventDefault();
+    window.location = 'https://socialsciences.uoregon.edu/anthropology';
+}, false);
+
